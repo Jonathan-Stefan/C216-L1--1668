3ce5bb1c9175e09d7588d6586c8d6faa50bf1ad2
jonathan <jonathan.stefan@inatel.br>
2024-02-27 21:24:00 -0300

    [Entrega] Pratica-0

---
+++ pratica-0/src/index.js
@@ -0,0 +1,9 @@
+// Exemplo de uso prático
+let nome = "Jonathan Stefan - 1668";
+function saudacaoPersonalizada(nome) {
+    console.log("Olá, " + nome + "!");
+}
+saudacaoPersonalizada(nome);
+
+let modulo = require('./module.js');
+modulo.saudacao();
---
+++ pratica-0/src/module.js
@@ -0,0 +1,8 @@
+// arquivo modulo.js
+module.exports = {
+    mensagem: 'Testando modulo: Jonathan - 1668 !',
+    saudacao: function() {
+      console.log(this.mensagem);
+    }
+  };
+  
